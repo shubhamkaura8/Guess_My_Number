b55adff17da1930511c8b6fbef3a6659bf996d1e
Shubham Kaura <shubhamkaura8@gmail.com>
2023-12-26 19:30:43 -0700

    Add basic game logic

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🤷‍♂️No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🥳Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '👆Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😭You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' 👇Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😭You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
